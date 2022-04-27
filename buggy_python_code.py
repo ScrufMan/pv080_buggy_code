1f9c823f13cbdab9d9a5d59c6457b1a4cf803322
unknown <xdrabik@ad.fi.muni.cz>
2022-04-27 16:39:09 +0200

    somer errors fixed

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,5 +1,6 @@
 # contains bunch of buggy examples
-# taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
+# taken from
+# https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
 import cPickle
 import subprocess
 import base64
@@ -13,7 +14,7 @@ def transcode_file(request, filename):
 
 
 # Assert statements
-def foo(request, user):
+def foop(user):
     assert user.is_admin, 'user does not have access'
     # secure code...
 
@@ -32,4 +33,4 @@ def index():
     import_urlib_version(module)
 
 
-print(base64.b64encode(pickle.dumps(RunBinSh())))
+print(base64.b64encode(cPickle.dumps(RunBinSh())))
